008387fac1d476fb10e13d0656c76f636dbab1de
stebssbets <32585154+stebssbets@users.noreply.github.com>
2019-04-16 21:05:20 -0400

    added BoombotSongScript, Boombot song plays at start (feel free to comment out), WIP: getting audio source data of Boombot song

---
+++ Assets/BoombotSongScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoombotSongScript : MonoBehaviour
+{
+    private AudioSource BoombotSong;
+    private AudioSource[] ArrayOfCameraAudioSources;
+    private float[] NumberOfBoombotSongSamples;
+    private int Channel;
+    private int secondsToWait;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ArrayOfCameraAudioSources = GetComponents<AudioSource>();
+        BoombotSong = ArrayOfCameraAudioSources[1];
+        Debug.Log(BoombotSong);
+        BoombotSong.Play();
+        BoombotSong.GetOutputData(NumberOfBoombotSongSamples,Channel);
+        //secondsToWait = 3;
+        //yield return new WaitForSeconds(secondsToWait);
+        Debug.Log(NumberOfBoombotSongSamples);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
