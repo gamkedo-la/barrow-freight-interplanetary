446ff1134f4497dabd3cdace9ef7dd3bd9933820
stebssbets <32585154+stebssbets@users.noreply.github.com>
2019-04-17 17:29:14 -0400

    WIP: getting 'under the hood' of the boombot audio source and audio clip from the main camera and boombotsong script to figure out accurate timing for song to dance of boombot, posted public variables to the camera object/boombot song script (current time, current sample, total time, total samples, speaker positions)

--- Assets/BoombotSongScript.cs
+++ Assets/BoombotSongScript.cs
@@ -4,28 +4,51 @@ using UnityEngine;
 
 public class BoombotSongScript : MonoBehaviour
 {
-    private AudioSource BoombotSong;
+    private AudioSource BoombotSongSource;
     private AudioSource[] ArrayOfCameraAudioSources;
-    private float[] NumberOfBoombotSongSamples;
+    
+    private AudioClip BoombotSongClip;
+
+    public float[] BoombotSongSpeakerPositions;
+    public int BoombotSongSample;
+    public float BoombotSongCurrentTime;
+    public float BoombotSongTimeLength;
+    public float BoombotSongTotalSamples;
+    
     private int Channel;
     private int secondsToWait;
 
     // Start is called before the first frame update
     void Start()
     {
-        ArrayOfCameraAudioSources = GetComponents<AudioSource>();
-        BoombotSong = ArrayOfCameraAudioSources[1];
-        Debug.Log(BoombotSong);
-        BoombotSong.Play();
-        BoombotSong.GetOutputData(NumberOfBoombotSongSamples,Channel);
+        ArrayOfCameraAudioSources = GetComponents<AudioSource>();        ;
+
+        BoombotSongSource = ArrayOfCameraAudioSources[1];
+        Debug.Log(BoombotSongSource);
+
+        BoombotSongClip = BoombotSongSource.clip;
+        BoombotSongTimeLength = BoombotSongClip.length;
+
+        BoombotSongTotalSamples = BoombotSongClip.samples;
+        
+
+        BoombotSongSource.Play();
+
         //secondsToWait = 3;
         //yield return new WaitForSeconds(secondsToWait);
-        Debug.Log(NumberOfBoombotSongSamples);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        //BoombotSongSpeakerPositions = BoombotSongSource.GetOutputData(1024, 0);
+        //Debug.Log(BoombotSongSpeakerPositions);
+        BoombotSongSample = BoombotSongSource.timeSamples;
+        Debug.Log(BoombotSongSample);
+        BoombotSongCurrentTime = BoombotSongSource.time;
+        Debug.Log(BoombotSongCurrentTime);
         
+
     }
 }
