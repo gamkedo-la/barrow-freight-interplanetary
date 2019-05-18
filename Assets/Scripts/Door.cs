fa0f7b41ff0d954d95b1af73996e3af839d7ab9d
Tyler Funk <tcfunk25@gmail.com>
2019-05-17 21:55:54 -0500

    script to trigger door animations

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        Debug.Log(anim);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("trigger enter");
+            anim.SetTrigger("Open");
+            anim.ResetTrigger("Close");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("trigger exit");
+            anim.SetTrigger("Close");
+            anim.ResetTrigger("Open");
+        }
+    }
+}
