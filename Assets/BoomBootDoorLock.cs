a12dd7da1a10528128011221182a171e4000596b
Brian Boucher <junkyardconcierge@gmail.com>
2019-05-26 12:35:56 -0500

    Added some event scripting functionallity to lock and unlock doors as part of the mission scripted events.

---
+++ Assets/BoomBootDoorLock.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoomBootDoorLock : MonoBehaviour
+{
+    Door door;
+    Jobs jobManager;
+
+    void Start() {
+        door = GameObject.Find("Door (3)").GetComponent<Door>();
+        jobManager = GameObject.Find("Game Managers").GetComponent<Jobs>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && jobManager.jobsCompleted == 1)
+        {
+            door.LockDoor();
+        }
+    }
+
+    //public void A()
+    //{
+    //    this.SetActive(true);
+    //}
+//
+    //public void SetInactive()
+    //{
+    //    this.SetActive(false);
+    //}
+}
